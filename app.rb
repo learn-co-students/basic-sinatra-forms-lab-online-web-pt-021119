d49aa9069800aa6aaa1bcce1b0cd2957bf289b5f
Berlinta <nashfft@gmail.com>
2019-05-23 18:03:02 -0700

    test

--- app.rb
+++ app.rb
@@ -3,4 +3,12 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+    get '/newteam' do        
+        erb:newteam
+    end
+
+    get '/team' do
+        @params = params
+        erb:team
+    end
 end
